f11f7557699953040e9c34c2eaea4b70370fc883
ethanfan <iosdev@linkgoo.cn>
2016-08-22 14:49:03 +0800

    cordova 框架

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+                  {
+                  "id": "cordova-plugin-console.console",
+                  "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+                  "pluginId": "cordova-plugin-console",
+                  "clobbers": [
+                               "console"
+                               ]
+                  },
+                  {
+                  "id": "cordova-plugin-console.logger",
+                  "file": "plugins/cordova-plugin-console/www/logger.js",
+                  "pluginId": "cordova-plugin-console",
+                  "clobbers": [
+                               "cordova.logger"
+                               ]
+                  },
+                  {
+                  "id": "cordova-plugin-autoUpdate.autoUpdate",
+                  "file": "plugins/cordova-plugin-autoUpdate/www/autoUpdate.js",
+                  "clobbers": [
+                               "checkUpdate"
+                               ]
+                  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-console": "1.0.3"
+};
+// BOTTOM OF METADATA
+});
