ff09bb31ed33e5520d1370bbabf389dcd3abd9f0
zs <371960474@qq.com>
2016-09-27 11:30:35 +0800

    JPUSH

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -23,7 +23,7 @@ module.exports.metadata =
     "cordova-plugin-console": "1.0.3",
     "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-device": "1.1.3",
-    "jpush-phonegap-plugin": "2.2.4"
+    "jpush-phonegap-plugin": "2.2.5"
 };
 // BOTTOM OF METADATA
 });
--- www/Controller/home.js
+++ www/Controller/home.js
@@ -32,57 +32,57 @@ function machine_input(type){
 function back() {
 	app.navigate("home.html");
 }
-// document.addEventListener("deviceready", function() {
-// 	window.plugins.jPushPlugin.init();
-//
-// 	// window.plugins.jPushPlugin.getRegistrationID(onGetRegistradionID);
-// 	// var onGetRegistradionID = function(data) {
-// 	// 	try {
-// 	// 		alert("JPushPlugin:registrationID is " + data);
-// 	// 	} catch(exception) {
-// 	// 		alert(exception);
-// 	// 	}
-// 	// }
-// 	window.plugins.jPushPlugin.isPushStopped(function (result) {
-// 		if (result == 0) {
-// 			// 开启
-// 			alert("开启");
-// 		} else {
-// 			// 关闭
-// 			alert("关闭");
-// 		}
-// 	})
-//
-// 	var onReceiveNotification = function(event) {
-// 		try {
-// 			var alertContent;
-// 			if (device.platform == "Android") {ƒ
-// 				alertContent = event.alert;
-// 			} else {
-// 				alertContent = event.aps.alert;
-// 			}
-// 			$("#notificationResult").html(alertContent);
-// 		} catch (exception) {
-// 			console.log(exception)
-// 		}
-// 	};
-//
-// 	var onReceiveMessage = function(event) {
-// 		try {
-// 			var message;
-// 			if (device.platform == "Android") {
-// 				message = event.message;
-// 			} else {
-// 				message = event.content;
-// 			}
-// 			$("#messageResult").html(message);
-// 		} catch (exception) {
-// 			console.log("JPushPlugin:onReceiveMessage-->" + exception);
-// 		}
-// 	};
-// 	document.addEventListener("jpush.receiveNotification", onReceiveNotification, false);
-// 	document.addEventListener("jpush.receiveMessage", onReceiveMessage, false);
-// }, false);
+document.addEventListener("deviceready", function() {
+	window.plugins.jPushPlugin.init();
+
+	window.plugins.jPushPlugin.getRegistrationID(onGetRegistradionID);
+	var onGetRegistradionID = function(data) {
+		try {
+			alert("JPushPlugin:registrationID is " + data);
+		} catch(exception) {
+			alert(exception);
+		}
+	}
+	window.plugins.jPushPlugin.isPushStopped(function (result) {
+		if (result == 0) {
+			// 开启
+			alert("开启");
+		} else {
+			// 关闭
+			alert("关闭");
+		}
+	})
+
+	var onReceiveNotification = function(event) {
+		try {
+			var alertContent;
+			if (device.platform == "Android") {ƒ
+				alertContent = event.alert;
+			} else {
+				alertContent = event.aps.alert;
+			}
+			$("#notificationResult").html(alertContent);
+		} catch (exception) {
+			console.log(exception)
+		}
+	};
+
+	var onReceiveMessage = function(event) {
+		try {
+			var message;
+			if (device.platform == "Android") {
+				message = event.message;
+			} else {
+				message = event.content;
+			}
+			$("#messageResult").html(message);
+		} catch (exception) {
+			console.log("JPushPlugin:onReceiveMessage-->" + exception);
+		}
+	};
+	document.addEventListener("jpush.receiveNotification", onReceiveNotification, false);
+	document.addEventListener("jpush.receiveMessage", onReceiveMessage, false);
+}, false);
 
 
 
