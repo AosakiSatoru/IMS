f8ae48f5eb799b414ef7d1017605d0b383a70fac
ethanfan <iosdev@linkgoo.cn>
2016-09-29 18:20:40 +0800

    告警详细页面只剩消息子类别未完成

--- www/Controller/warningDetail.js
+++ www/Controller/warningDetail.js
@@ -2,6 +2,8 @@
  * Created by Satoru on 16/9/20.
  */
 
+var workingMark = false;
+
 function viewShow(e) {
 	var navbar = $("#navbar").kendoMobileNavBar();
 	warningDetailInitRequest(e.view.params.deviceCode);
@@ -45,40 +47,54 @@ function warningDetailBindView(data, devcode) {
 		$("#warningDetail_ArticleName").text(data.outputstr.ArticleName);
 		$("#warningDetail_EfficProduction").text(data.outputstr.EfficProduction);
 		var alarmrows = data.outputstr.alarmrows;
+
 		$.each(alarmrows, function(n, value) {
 			var messageid = value.messageid;
 			switch(messageid) {
 				case "0":
 					{
-						var txt = $("#warningDetail_deviceStatusMessage").text() + value.message + "<br>";
-						$("#warningDetail_deviceStatusMessage").html(txt);
+						if(!workingMark) {
+							workingMark = true;
+							$("#warningDetail_deviceStatusMessage").html(value.message + "<br>");
+						} else {
+							var txt = $("#warningDetail_deviceStatusMessage").html() + value.message + "<br>";
+							$("#warningDetail_deviceStatusMessage").html(txt);
+						}
 					}
 					break;
 				case "1":
 					{
 						//不处理
-						var category = new Array();
-						category.push({
-							"otherMessage": value.alarmtime + " " + value.message
-						});
-						dataSource = kendo.data.DataSource.create({
-							data: category,
-						});
-
-						var contentViewModel = kendo.observable({
-							"dataSource": dataSource,
-
-						});
-						kendo.bind($("#warningDetailListView"), contentViewModel);
 					}
 					break;
 				case "2":
+					{
+						switch(value.messagechild) {
+							case "01":
+								{
+									if(!workingMark) {
+										workingMark = true;
+										$("#warningDetail_QualityMessage_1").html(value.message + "<br>");
+										$("#warningDetail_QualityMessageTitle_1").text("组切割");
+									} else {
+										var txt = $("#warningDetail_QualityMessage_1").html() + value.message + "<br>";
+										$("#warningDetail_QualityMessage_1").html(txt);
+									}
+								}
+								break;
+							default:
+								{
+
+								}
+								break;
+						}
+					}
 					break;
 				case "3":
 					{
 						var category = new Array();
 						category.push({
-							"otherMessage": value.message
+							"otherMessage": new Date(value.alarmtime).Format("yyyy-MM-dd hh:mm") + "&nbsp&nbsp&nbsp" + value.message
 						});
 						dataSource = kendo.data.DataSource.create({
 							data: category,
@@ -98,5 +114,24 @@ function warningDetailBindView(data, devcode) {
 					break;
 			}
 		});
+		workingMark = false;
 	}
 }
+
+Date.prototype.Format = function(fmt) { //author: meizz  
+	var o = {
+		"M+": this.getMonth() + 1, //月份  
+		"d+": this.getDate(), //日  
+		"h+": this.getHours(), //小时  
+		"m+": this.getMinutes(), //分  
+		"s+": this.getSeconds(), //秒  
+		"q+": Math.floor((this.getMonth() + 3) / 3), //季度  
+		"S": this.getMilliseconds() //毫秒  
+	};
+	if(/(y+)/.test(fmt))
+		fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+	for(var k in o)
+		if(new RegExp("(" + k + ")").test(fmt))
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+	return fmt;
+};
