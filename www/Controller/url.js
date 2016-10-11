900329f0755c051469d99fc54bc68c67ee7f1f51
zs <371960474@qq.com>
2016-10-11 10:55:24 +0800

    高明地址加上端口

--- www/Controller/url.js
+++ www/Controller/url.js
@@ -1,4 +1,4 @@
 //公司
-var IMSUrl = "http://10.0.0.254:8080/service/ws/api/rest/";
+//var IMSUrl = "http://10.0.0.254:8080/service/ws/api/rest/";
 //高明
-//var IMSUrl = "http://192.168.75.67/service/ws/api/rest/";
+var IMSUrl = "http://192.168.75.67:8080/service/ws/api/rest/";
