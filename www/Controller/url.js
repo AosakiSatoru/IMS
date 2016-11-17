a8b80ee6bb3b923a286e3ff411b88a65a497d9b3
zs <371960474@qq.com>
2016-11-17 13:11:38 +0800

    程序瘦身

--- www/Controller/url.js
+++ www/Controller/url.js
@@ -1,6 +1,6 @@
 //DEV
 //var IMSUrl = "http://10.0.0.33:8080/service/ws/api/rest/";
 //公司
-var IMSUrl = "http://10.0.0.254:8080/service/ws/api/rest/";
+//var IMSUrl = "http://10.0.0.254:8080/service/ws/api/rest/";
 //高明
-//var IMSUrl = "http://192.168.75.67:8080/service/ws/api/rest/";
+var IMSUrl = "http://192.168.75.67:8080/service/ws/api/rest/";
